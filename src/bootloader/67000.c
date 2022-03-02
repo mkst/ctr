7f33c9bdac0ab3ba890cb3cd3b5fe84e2d467911
Mark Street <streetster@gmail.com>
2022-03-02 18:32:02 +0000

    Updated dependencies

--- src/bootloader/67000.c
+++ src/bootloader/67000.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "67000", SpuSetCommonCDReverb);
+INCLUDE_ASM("asm/bootloader/nonmatchings/67000", SpuSetCommonCDReverb);
 
-// INCLUDE_ASM(s32, "67000", func_80076840);
+// INCLUDE_ASM("asm/bootloader/nonmatchings/67000", func_80076840);
 void SpuSetReverbVoice(s32 arg0, s32 arg1) {
     _SpuSetAnyVoice(arg0, arg1, 204, 205);
 }
